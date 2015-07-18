d2c6fb3ffe0cedb5bd01c77d8b8e43dd45e8cbab
Gerhard Lausser <gerhard.lausser@consol.de>
2015-07-18 23:48:38 +0200

    finish the first release

--- check_stable.go
+++ check_stable.go
@@ -9,56 +9,74 @@ import (
   "crypto/sha1"
   "encoding/json"
   "io/ioutil"
-  //"syscall"
+  "syscall"
 )
 
 type Result struct {
   // Dreck! Die Felder muessen mit Grossbuchstaben anfangen
   // da sie sonst der feine Herr Golang nicht exportiert
   Output string `json:"output"`
-  Exitcode int `json:"exitcode"`
+  ExitCode int `json:"exitcode"`
   Serial int `json:"serial"`
+}
+
+func match() {
+}
+
+func loadResult(resultFile string) Result {
+  var result Result;
+  raw, err := ioutil.ReadFile(resultFile)
+  if err != nil {
+    result = Result{"", 0, 0}
+  } else {
+    json.Unmarshal(raw, &result)
+  }
+  return result
+}
+
+func saveResult(resultFile string, result Result) {
+  json_result, err := json.Marshal(result)
+  if err != nil {
+    fmt.Println("json error:", err)
+  }
+  err = ioutil.WriteFile(resultFile, json_result, 0644)
+  if err != nil {
+    fmt.Printf("could not write resultFile %s\n", err)
+  }
+}
+
+func initResultFile(args string) string {
+  var resultDir string;
+  sha1_hash := fmt.Sprintf("%x", sha1.Sum([]byte(args)))
+  if (os.Getenv("OMD_ROOT") != "") {
+    resultDir = filepath.Join(os.Getenv("OMD_ROOT"),
+        "var", "tmp", "check_stable");
+
+  } else {
+    resultDir = filepath.Join("/", "var", "tmp", "check_stable");
+  }
+  os.MkdirAll(resultDir, 0755)
+  return filepath.Join(resultDir, sha1_hash)
 }
 
 func main() {
   args := os.Args
   cmd := args[1]
   params := args[2:len(args)]
-  var statefilesdir string;
-  var result Result
+  exitCode := 0
 
-  sha1_hash := fmt.Sprintf("%x", sha1.Sum([]byte(strings.Join(args, ""))))
-  if (os.Getenv("OMD_ROOT") != "") {
-    statefilesdir = filepath.Join(os.Getenv("OMD_ROOT"),
-        "var", "tmp", "stabilize");
+  resultFile := initResultFile(strings.Join(args, ""))
+  plugin := exec.Command(cmd, params...)
 
+  pluginOutput, err := plugin.CombinedOutput()
+  if err != nil {
+    if exitError, ok := err.(*exec.ExitError); ok {
+      exitCode = exitError.Sys().(syscall.WaitStatus).ExitStatus()
+    }
   } else {
-    statefilesdir = filepath.Join("/", "var", "tmp", "stabilize");
+    exitCode = plugin.ProcessState.Sys().(syscall.WaitStatus).ExitStatus()
   }
-  os.MkdirAll(statefilesdir, 0755)
-  statefile := filepath.Join(statefilesdir, sha1_hash)
 
-  output, err := exec.Command(cmd, params...).CombinedOutput()
-
-  //if err := cmd.Start(); err != nil {
-    //log.Fatalf("cmd.Start: %v")
-  //}
-
-  //if err := cmd.Wait(); err != nil {
-    //if exiterr, ok := err.(*exec.ExitError); ok {
-      // The program has exited with an exit code != 0
-
-      // There is no plattform independent way to retrieve
-      // the exit code, but the following will work on Unix
-      //if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
-        //log.Printf("Exit Status: %d", status.ExitStatus())
-      //}
-    //} else {
-      //log.Fatalf("cmd.Wait: %v", err)
-    //}
-  //}
-
-  fmt.Printf("The date is %s\n", output)
   var errors = []string{
       "Return code of 127 is out of bounds - plugin may be missing",
       "Return code of 254 is out of bounds",
@@ -69,46 +87,31 @@ func main() {
   }
   var error_found = false
   for i:= 0; i < len(errors); i++ {
-    if strings.Index(string(output), errors[i]) != -1 {
+    if strings.Index(string(pluginOutput), errors[i]) != -1 {
       error_found = true
-      fmt.Printf("plugin error ->%s\n", errors[i])
       break
     }
   }
 
-  raw, err := ioutil.ReadFile(statefile)
-  if err != nil {
-    result = Result{string(output), 0, 0}
-    // error
-  } else {
-    json.Unmarshal(raw, &result)
-  }
-
-  // load json, output, serial
-  // else record.output = output, recore.serial = max+1
   if error_found {
-    fmt.Printf("no error write to %s\n", statefile)
-    // if serial < max, print output, save output with serial++
-    // else print error output
-    if result.Serial > 2 {
-      fmt.Printf("%s\n", output)
+    // plugin ended with one of these high-load side effects
+    lastResult := loadResult(resultFile)
+    if lastResult.Serial > 1 {
+      // the error did not go away, time to alert
+      fmt.Printf("%s\n", pluginOutput)
+      os.Exit(exitCode)
+    } else {
+      // output a fake result and increment the counter
+      saveResult(resultFile, Result{ lastResult.Output, lastResult.ExitCode, lastResult.Serial + 1 })
+      fmt.Printf("(stabilized #%d) %s\n", lastResult.Serial, lastResult.Output)
+      os.Exit(lastResult.ExitCode)
     }
   } else {
-    // save output, 
-    fmt.Printf("no plugin error write good %s\n", statefile)
-    // save with serial 0
-    // record.serial = 0
-    json_result, err := json.Marshal(result)
-    fmt.Printf("file written %s\n", string(json_result))
-    if err != nil {
-      fmt.Println("error:", err)
-    }
-    err = ioutil.WriteFile(statefile, json_result, 0644)
-    if err != nil {
-      fmt.Printf("file write fail %s\n", err)
-    } else {
-      fmt.Printf("file written %s\n", statefile)
-    }
+    // save the output, plugin had no problems
+    saveResult(resultFile, Result{ string(pluginOutput), exitCode, 0 })
+    // exit with the original plugin result
+    fmt.Printf("%s\n", pluginOutput)
+    os.Exit(exitCode)
   }
 }
 
